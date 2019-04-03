c5002e74816afe82edd5ec26efebeeac40536e67
kyobix <ddenis1994@gmail.com>
2019-04-03 18:29:52 +0300

    add security

--- DBLocal/database.py
+++ DBLocal/database.py
@@ -7,6 +7,8 @@ engine = create_engine('sqlite:////tmp/test.db', \
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 Base.query = db_session.query_property()
 
--- project.py
+++ project.py
@@ -1,23 +1,24 @@
-from flask import Flask,template_rendered
+from flask import Flask, template_rendered, flash
 from flask_security import Security, login_required, \
      SQLAlchemySessionUserDatastore
-from DBLocal.database import db_session, init_db
+from DBLocal.database import db_session, init_db, Session
 from DBLocal.models import User, Role
+from flask_sqlalchemy import SQLAlchemy
 
 # Create app
 app = Flask(__name__)
-
 # Setup Flask-Security
 user_datastore = SQLAlchemySessionUserDatastore(db_session,
                                                 User, Role)
 security = Security(app, user_datastore)
-
+session = Session()
 
 # Create a user to test with
 @app.before_first_request
 def create_user():
     init_db()
-    user_datastore.create_user(email='matt@nobien.net', password='password')
+    #session.add(User(username='admin', email='admin@localhost'))
+    #user_datastore.create_user(email='matt@nobien.net', password='password')
     db_session.commit()
 
 
@@ -25,7 +26,19 @@ def create_user():
 @app.route('/')
 @login_required
 def home():
+    result = User.query.filter_by(username='admin').first()
     return template_rendered('Here you go!')
+
+
+@app.route('/login_submit')
+def login_submit(user_name, password):
+    result = User.query.filter_by(username=user_name).first()
+    if result == "None":
+        flash("wrong user name", category="login")
+        return home()
+    if result.password == password:
+        return index()
+
 
 
 if __name__ == '__main__':
